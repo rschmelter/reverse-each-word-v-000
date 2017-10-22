02de74c5992d157faef58c604ff26035b3f35be0
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-22 19:43:29 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+#
+# def reverse_each_word(string)
+#   array = string.split
+#   reversed_string = []
+#   array.each do |word|
+#     reversed_string << word.reverse
+#   end
+#   reversed_string.join(" ")
+# end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |word|
+    word.reverse
+  end
+end
